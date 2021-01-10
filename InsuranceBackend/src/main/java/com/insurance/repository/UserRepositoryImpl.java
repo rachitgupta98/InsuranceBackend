35df71e9632df1f6672c56762a5cfad87a13c710
rachitgupta98 <rachit371@gmail.com>
2021-01-10 11:55:15 +0530

    removed existing code

--- InsuranceBackend/src/main/java/com/insurance/repository/UserRepositoryImpl.java
+++ InsuranceBackend/src/main/java/com/insurance/repository/UserRepositoryImpl.java
@@ -14,33 +14,28 @@ import com.insurance.entities.User;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
+
 	@PersistenceContext
 	EntityManager em;
-	
+
 	@Transactional
 	public long signUpUser(User user) {
-		// TODO Auto-generated method stub
-		User newUser=em.merge(user);
-		return newUser.getUserId();
+		return 0;
 	}
 
 	@Transactional
 	public List<User> viewAllUsers() {
-		String jpql="select u from User u";
-		Query query=em.createQuery(jpql);
-		List<User> users=query.getResultList();
-		return users;
+		
+		return null;
 	}
 
 	@Override
 	public User findUserById(long userId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public User deleteUser(long userId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
--- InsuranceBackend/src/main/java/com/insurance/repository/VehicleRepositoryImpl.java
+++ InsuranceBackend/src/main/java/com/insurance/repository/VehicleRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.insurance.repository;
 
 import java.util.List;
 
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -16,32 +15,22 @@ import com.insurance.entities.Vehicle;
 public class VehicleRepositoryImpl implements VehicleRepository {
 	@PersistenceContext
 	EntityManager em;
-	
+
 	@Transactional
 	public long addOrUpdateVehicle(Vehicle vehicle) {
-		// TODO Auto-generated method stub
-		Vehicle newVehicle=em.merge(vehicle);
-		return newVehicle.getVehicleId();
+		return 0;
 	}
 
 	@Transactional
 	public List<Vehicle> viewAllvehicles() {
-		// TODO Auto-generated method stub
-		String jpql="select v from Vehicle v";
-		Query query=em.createQuery(jpql);
-		
-		List<Vehicle> vehicles=query.getResultList();
-		return vehicles;
+
+		return null;
 	}
 
 	@Transactional
 	public Vehicle findVehicleById(String registrationNumber) {
-		// TODO Auto-generated method stub
-		String jpql="select v from Vehicle v where v.registrationNumber=:registrationNumber";
-		Query query=em.createQuery(jpql);
-		query.setParameter("registrationNumber", registrationNumber);
-		Vehicle vehicle=(Vehicle)query.getSingleResult();
-		return vehicle;
+
+		return null;
 	}
 
 }
