3ea1631634e4d876eff2904e12f306369cef92f9
GvsSaiSumanth <gvssaisumanth24@gmail.com>
2021-01-15 15:04:47 +0530

    paches

--- InsuranceBackend/src/main/java/com/insurance/service/EmailService.java
+++ InsuranceBackend/src/main/java/com/insurance/service/EmailService.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.insurance.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,28 +22,4 @@ public class EmailService {
     }
 
 }
-=======
-package com.insurance.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailService {
-
-	@Autowired
-    private MailSender mailSender;
-    public void sendEmail(String email,String text,String subject) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("InsuranceGroup13@outlook.com");
-        message.setTo(email);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
-
-    }
-
-}
->>>>>>> branch 'master' of https://github.com/rachitgupta98/InsuranceBackend.git
